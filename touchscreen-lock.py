78d6b1ea757b55e5dede9dcbb27469f6d83c70c8
Robin SHAMSNEJAD <rsham@free.fr>
2020-06-11 14:37:34 +0200

    Fixed fullscreen which prevented keypresses

--- touchscreen-lock.py
+++ touchscreen-lock.py
@@ -6,11 +6,10 @@ from PIL import Image, ImageTk
 def showPIL(pilImage):
     root = tkinter.Tk()
     w, h = root.winfo_screenwidth(), root.winfo_screenheight()
-    root.overrideredirect(1)
     root.geometry("%dx%d+0+0" % (w, h))
-    root.focus_set()
     root.attributes('-type', 'normal')
     root.attributes('-alpha', 0.5)
+    root.attributes('-fullscreen', 'True')
     root.bind("<Escape>", lambda e: (e.widget.withdraw(), e.widget.quit()))
     canvas = tkinter.Canvas(root,width=w,height=h)
     canvas.pack()
@@ -23,6 +22,7 @@ def showPIL(pilImage):
         pilImage = pilImage.resize((imgWidth,imgHeight), Image.ANTIALIAS)
     image = ImageTk.PhotoImage(pilImage)
     imagesprite = canvas.create_image(w/2,h/2,image=image)
+    root.focus_set()
     root.mainloop()
 
 im = Image.open('./padlock.png')
